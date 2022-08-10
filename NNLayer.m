5dd223096900e16c135ee1105c6b5ed271df41ce
sitdownplz <rul3xji4@gmail.com>
2022-08-10 18:00:20 +0800

    2022/08/10
    1. 修改 Forward function 以擴展後續 backward更新接口

--- NNLayer.m
+++ NNLayer.m
@@ -1,11 +1,11 @@
-classdef NNLayer
+classdef NNLayer < handle
 
     properties (Access=public)
         X {mustBeNumeric}    % Input data
         neuronNum {mustBeNumeric}    % Number of neurons
         W {mustBeNumeric}    % Weight
         b {mustBeNumeric}    % Bias
-        actFcnMethod  {mustBeMember(actFcnMethod, {'tanh', 'sigmoid'})}= 'tanh'    % Activation funcion method
+        actFcnMethod  {mustBeMember(actFcnMethod, {'tanh', 'sigmoid', 'none'})}= 'tanh'    % Activation funcion method
         actFcn = @tanh    % Activation function
     end
 
@@ -17,27 +17,41 @@ classdef NNLayer
             if nargin < 2
                 self.W = ones(inputSize, 1);
                 self.b = zeros(inputSize, 1);
-            elseif nargin >2
+            elseif nargin > 2
                 self.actFcnMethod = actFcnMethod;
                 switch actFcnMethod
                     case 'tanh'
                         self.actFcn = @tanh;
                     case 'sigmoid'
                         self.actFcn = @(x) 1./(1+e.^x);
+                    case 'none'
+                        self.actFcn = @(x)x;
                 end
+                self.W = rand(outputSize, inputSize);
+                self.b = rand(outputSize,1);
             else
                 self.W = rand(outputSize, inputSize);
                 self.b = rand(outputSize,1);
             end
         end
 
-        function y = forward(self, x)
+        function y = forward(self, x, varargin)
             self.X = x(:);
             if size(self.X,1) ~= self.neuronNum
                 error('Input size must equal to neurons')
             end
-            y = self.actFcn(self.W * self.X + self.b);
+            if isempty(varargin)
+                y = self.actFcn(self.W * self.X + self.b);
+            else
+                switch inputname(2)
+                    case 'W_temp'
+                        y = self.actFcn(varargin{2} * self.X + self.b);
+                    case 'b_temp'
+                        y = self.actFcn(self.W * self.X + varagin);
+                end
+            end
         end
+
 
 
     end
