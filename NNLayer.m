7d6ec0ed7af4cfb0908fb349141cf45d36a14bff
sitdownplz <rul3xji4@gmail.com>
2022-08-22 10:43:17 +0800

    2022/08/22
    1. Add hidden state property
    2. Update single neuron layer definition
    3. Update forward function, add hidden value caculate and manual input X data, weight and bias

--- NNLayer.m
+++ NNLayer.m
@@ -1,22 +1,25 @@
 classdef NNLayer < handle
-
+    % Create default neural network layer framework
+    % Layer Object = NNLayer(input neuron number, output neuron number, activation funciton)
     properties (Access=public)
         X {mustBeNumeric}    % Input data
         neuronNum {mustBeNumeric}    % Number of neurons
         W {mustBeNumeric}    % Weight
         b {mustBeNumeric}    % Bias
-        actFcnMethod  {mustBeMember(actFcnMethod, {'tanh', 'sigmoid', 'none'})}= 'tanh'    % Activation funcion method
+        h {mustBeNumeric}    % Hidden State
+        actFcnMethod {mustBeMember(actFcnMethod, {'tanh', 'sigmoid', 'none'})}= 'tanh'    % Activation funcion method
         actFcn = @tanh    % Activation function
     end
 
     methods
         function self = NNLayer(inputSize, outputSize, actFcnMethod)
-
             self.neuronNum = inputSize;
-
-            if nargin < 2
-                self.W = ones(inputSize, 1);
-                self.b = zeros(inputSize, 1);
+            % Currently, only allow single neural in the output layer
+            if outputSize == 1  %TODO
+                self.W = rand(1,inputSize);
+                self.b = zeros(outputSize, 1);
+                self.actFcnMethod = 'none';
+                self.actFcn = @(x)x;
             elseif nargin > 2
                 self.actFcnMethod = actFcnMethod;
                 switch actFcnMethod
@@ -33,21 +36,28 @@ classdef NNLayer < handle
                 self.W = rand(outputSize, inputSize);
                 self.b = rand(outputSize,1);
             end
+
         end
 
+        % Forward function
         function y = forward(self, x, varargin)
-            self.X = x(:);
-            if size(self.X,1) ~= self.neuronNum
+            if size(x,1) ~= self.neuronNum
                 error('Input size must equal to neurons')
             end
             if isempty(varargin)
+                self.X = x;
                 y = self.actFcn(self.W * self.X + self.b);
+                self.h = self.W * self.X + self.b;
             else
                 switch inputname(2)
-                    case 'W_temp'
-                        y = self.actFcn(varargin{2} * self.X + self.b);
-                    case 'b_temp'
-                        y = self.actFcn(self.W * self.X + varagin);
+                    case 'X_temp'   % Manul input X data
+                        y = self.actFcn(self.W * x + self.b);
+                    case 'W_temp'   % Manul input weight data
+                        y = self.actFcn(x * self.X + self.b);
+                        self.h = self.W * self.X + self.b;
+                    case 'b_temp'   % Manul input bias data
+                        y = self.actFcn(self.W * self.X + x);
+                        self.h = self.W * self.X + self.b;
                 end
             end
         end
